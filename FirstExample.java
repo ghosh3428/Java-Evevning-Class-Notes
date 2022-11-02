b2b28a6ce19f1bc21afdfcae45973becfa9251fc
ghosh3428 <ghosh.debabrato@gmail.com>
2022-11-02 20:41:33 +0530

    Add files via upload

---
+++ FirstExample.java
@@ -0,0 +1,14 @@
+class FirstExample
+{
+    public static void main(String [] args)
+    {
+        int first = 10;
+        int second = 3;
+
+        System.out.println("10 + 3 = " + (first + second));
+        System.out.println("10 - 3 = " + (first - second));
+        System.out.println("10 * 3 = " + (first * second));
+        System.out.println("10 / 3 = " + (first / second));
+        System.out.println("10 % 3 = " + (first % second));
+    }
+}
---
+++ IfElseExample.java
@@ -0,0 +1,16 @@
+class IfElseExample
+{
+    public static void main(String [] args)
+    {
+       int age = 25;
+
+       if(age > 18)
+       {
+            System.out.println("I can Drive.");
+       }
+       else
+       {
+            System.out.println("Since i am underage i cannot drive.");
+       }
+    }
+}
